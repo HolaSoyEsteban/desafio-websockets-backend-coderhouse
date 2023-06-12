5720efced6108078376b860886aff181e87ba6d4
Esteban Cardozo <cardozo.esteban@gmail.com>
2023-06-12 04:55:38 -0300

    Los productos se agregan desde el formulario de la ruta '/realtimeproducts' ...Listo!

--- src/app.js
+++ src/app.js
@@ -1,18 +1,41 @@
 import express from 'express'
-import fs from 'fs'
+import { Server } from 'socket.io'
+import handlebars from 'express-handlebars'
 import productsRouter from './routers/products.router.js'
 import cartsRouter from './routers/carts.router.js'
+import viewsRouter from './routers/views.router.js'
 
-const app = express(); // crea una instancia de una aplicación de express
 const PORT = 8080; // puerto en el que va a escuchar el servidor
 
-app.use(express.static('./public')); // middleware para servir archivos estáticos
+const app = express(); // crea una instancia de una aplicación de express
 app.use(express.json()); // middleware para parsear el body de las requests a JSON
+app.use(express.static('./public')); // middleware para servir archivos estáticos
+
+const serverHttp = app.listen(PORT, () => console.log('server up')) // levanta el servidor en el puerto especificado
+const io = new Server(serverHttp) // instancia de socket.io
+
+app.use((req, res, next) => {
+    req.io = io
+    next()
+}) // middleware para pasar la instancia de socket.io a los routers
 
 
+// configuracion del motor de plantillas handlebars
+app.engine('handlebars', handlebars.engine());
+app.set('views', './src/views');
+app.set('view engine', 'handlebars');
+
+
+app.get('/', (req, res) => res.render('index')); // ruta raíz
+
+app.use('/products', viewsRouter); // ruta para renderizar la vista de productos
 app.use('/api/products', productsRouter); // registra el router de productos en la ruta /api/products
 app.use('/api/carts', cartsRouter); // registra el router de carritos en la ruta /api/carts
 
-
-app.listen(PORT, () => console.log('server up')) // levanta el servidor en el puerto especificado
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado!');
+    socket.on('productList', (data) => {
+        io.emit('updatedProducts', data)
+    }) // evento que se ejecuta cuando se emite el evento productList
+}) // evento que se ejecuta cuando un cliente se conecta
 
